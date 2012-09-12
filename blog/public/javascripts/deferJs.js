074b0f3c8bc1f11d73a3638820509643a52ad179
Ruddy <ruddy@prophessi-Dell-DM061.(none)>
2012-09-12 14:45:23 +0200

    Adding jquery-1.8.1.min.js, disable deferJs

--- blog/public/javascripts/deferJs.js
+++ blog/public/javascripts/deferJs.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
+function createScriptElement(src){
+	var scpt=document.createElement('script');
+	scpt.type='text/javascript';
+	scpt.src=src;
+	//scpt.async='true';
+	return scpt;
+}
+
+//$(document).ready(function(){
 	//script(src='/javascripts/socket.io.min.js')
     //script(src='http://code.jquery.com/jquery-1.8.1.min.js')
-	$( document.createElement('script') ).attr('src','/javascripts/socket.io.min.js');
-	$( document.createElement('script') ).attr('src','http://code.jquery.com/jquery-1.8.1.min.js');
-	$( document.createElement('script') ).attr('src','/javascripts/index.js');
-});
+	//$( document.createElement('script') ).attr('src','/javascripts/socket.io.min.js');
+	//$( document.createElement('script') ).attr('src','http://code.jquery.com/jquery-1.8.1.min.js');
+	//$( document.createElement('script') ).attr('src','/javascripts/index.js');
+
+	//document.body.appendChild(createScriptElement('/javascripts/socket.io.min.js'));
+	//document.body.appendChild(createScriptElement('http://code.jquery.com/jquery-1.8.1.min.js'));
+//});
